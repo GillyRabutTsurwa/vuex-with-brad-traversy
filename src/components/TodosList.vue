dc9736bb675fd3e60d3a6a12282e7aa56ab4782b
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-10-14 13:12:50 -0500

    be right back

---
+++ src/components/TodosList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      Todos will go here
+  </div>
+</template>
+
+<script>
+import {mapActions} from "vuex";
+
+export default {
+  name: "TodoList",
+  methods: mapActions(["fetchTodos"]),
+  created() {
+    this.fetchTodos();
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,10 +1,19 @@
+import axios from "axios";
+
 const state = {};
 
 const getters = {};
 
 const mutations = {};
 
-const actions = {};
+const actions = {
+	async fetchTodos() {
+		const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+		console.log(response);
+		const data = response.data;
+		console.log(data);
+	}
+};
 
 export default {
 	state: state,
