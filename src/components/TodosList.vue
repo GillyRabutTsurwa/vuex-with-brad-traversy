dc46a861ba239922eed2a752b6b332a85e36e70c
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-10-14 18:56:54 -0500

    je rarrive

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -1,22 +1,51 @@
 <template>
   <div>
-      Todos will go here
+    <h3>Todos</h3>
+    <div class="todo-list">
+      <div v-for="currentTodo in allTodosArr" v-bind:key="currentTodo.id" class="todo-list__todo">
+        {{currentTodo.title}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import {mapActions} from "vuex";
-
 export default {
-  name: "TodoList",
-  methods: mapActions(["fetchTodos"]),
+  name: "TodosList",
+  computed: {
+    allTodosArr() {
+      return this.$store.getters.allTodos;
+    },
+  },
+  /** NOTE:
+   * Du même façon, actions are accessed using METHODS in vue. On va utiliser mapActions plus tard
+   */
+  methods: {
+    getTodos() {
+      // NOTENEWIMPORTANT: actions are triggered using the dispatch().
+      this.$store.dispatch("fetchTodos");
+    },
+  },
   created() {
-    this.fetchTodos();
-  }
-  
-}
+    console.log(this.$store);
+    this.getTodos();
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.todo-list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+}
+.todo-list__todo {
+  position: relative;
+  border: 1px solid #ccc;
+  background-color: #41b883;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  text-align: center;
+  cursor: pointer;
+}
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,23 +1,38 @@
 import axios from "axios";
 
-const state = {};
+const state = {
+  todos: [],
+};
 
-const getters = {};
+const getters = {
+  allTodos: (state) => state.todos,
+};
 
-const mutations = {};
+// mutations are responsible for reactively changing our state.
+// mutations, just like getters, accept the state as the first argument.
+// the second argument is the value that the original state might be changing to. This is formally known as a payload.
+// IMPORTANT: mutations are called using actions (which are below)
+// IMPORTANT: therefore think of the payload as a parametre. So when we call our mutation using an action, this payload corresponds to the data that we want to pass to the mutation, the payload, which is then the data that changes the original state.
+const mutations = {
+  // add todos that we are fetching from our action fetchTods() to our state, and putting it in our state.todos array
+  setTodos: (state, todosPayLoad) => (state.todos = todosPayLoad),
+};
 
+// actions are used for calling mutations. actions are generally functions that call the mutation, along with the data that we want to pass through.
+// we do this using the context parametre, which is an object that gives us access to the store, and we call the commit method that takes in 1. the name of the mutation (exact), and 2. the data we want to pass through.
 const actions = {
-	async fetchTodos() {
-		const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
-		console.log(response);
-		const data = response.data;
-		console.log(data);
-	}
+  async fetchTodos(context) {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    console.log(response);
+    const data = response.data;
+    console.log(data);
+    context.commit("setTodos", data);
+  },
 };
 
 export default {
-	state: state,
-	getters: getters,
-	mutations: mutations,
-	actions: actions
+  state: state,
+  getters: getters,
+  mutations: mutations,
+  actions: actions,
 };
