a0e13e382eccb874f68f68c701b0f09ed79f0a28
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-11-21 15:12:11 -0600

    reviewing my code. There are no significant changes in here either

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -23,6 +23,7 @@ export default {
     },
   },
   created() {
+    console.log(this.$store);
     console.log(this.allTodosArr);
   },
 };
--- src/store/store.js
+++ src/store/store.js
@@ -2,15 +2,12 @@ import Vuex from "vuex";
 import Vue from "vue";
 import todos from "./modules/todos";
 
-// Load Vuex
 Vue.use(Vuex);
 
-// Create store
 const store = new Vuex.Store({
-	modules: {
-		todos: todos
-	}
+  modules: {
+    todos: todos,
+  },
 });
 
-// Export the store where we will use in the app entry point, main.js
 export default store;
